c901614bce3c480be9a1613d64a110a56255482e
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-02-08 13:53:25 +0600

    Added a design to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,13 +4,32 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div>
-      <p className="text-3xl text-center font-bold">AboutPage</p>
-      <li className="list-none">
-        Go to{" "}
-        <Link href="/about/address" className="underline">
-          Address Page
-        </Link>
-      </li>
+      <section className="flex items-center gap-2">
+        <div className="w-1/2 h-auto border-2 p-2 flex items-center gap-2">
+          <img
+            src="https://i.ibb.co.com/cST4jyGP/working-person.png"
+            className=" w-1/2 mx-auto -mb-32"
+          />
+          <img
+            src="https://i.ibb.co.com/cST4jyGP/working-person.png"
+            className=" w-1/2 mx-auto"
+          />
+        </div>
+        <div className="w-1/2 p-2">
+          <h2 className="text-4xl font-bold">
+            Empowering Each Other to Succeed
+          </h2>
+          <p className="font-normal text-xl">
+            Every project we've undertaken has been a collaborative effort,
+            where every person involved has left their mark. Together, we've not
+            only constructed buildings but also built enduring connections that
+            define our success story.
+          </p>
+          <button className="text-lg bg-blue-400 px-2 py-3 rounded-md text-white mt-5">
+            Learn More
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
